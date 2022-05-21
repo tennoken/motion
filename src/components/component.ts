f5e3058fbbb8ff2bb80e264eaf4e24f4f715d58a
Kzero <tennoken@naver.com>
2022-05-21 14:30:13 +0900

    add item remove function

--- src/components/component.ts
+++ src/components/component.ts
@@ -1,5 +1,6 @@
 export interface Component {
 	attachTo(parent: HTMLElement, position?: InsertPosition): void;
+	removeFrom(parent: HTMLElement): void;
 }
 
 export class BaseComponent<T extends HTMLElement> implements Component {
@@ -13,5 +14,12 @@ export class BaseComponent<T extends HTMLElement> implements Component {
 
 	attachTo(parent: HTMLElement, position: InsertPosition = 'afterbegin') {
 		parent.insertAdjacentElement(position, this.element);
+	}
+
+	removeFrom(parent: HTMLElement): void {
+		if (parent !== this.element.parentElement) {
+			throw Error('Parent mismatch !');
+		}
+		parent.removeChild(this.element);
 	}
 }
